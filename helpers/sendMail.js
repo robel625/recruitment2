5c432fc1e4a4e5e514d52afe743ad644bd4530e9
Robel Gulima <robelgulima@gmail.com>
2022-08-06 17:58:10 +0300

    email secure false working 587

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -3,12 +3,12 @@ const nodemailer = require("nodemailer");
 
 export const sendEmail = (options) => {
 
-  // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD
